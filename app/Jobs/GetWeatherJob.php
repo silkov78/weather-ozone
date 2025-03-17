fa8001971b86ebba5ee323ef252c1ceae3f3b8e5
silkov78 <piotr.silkov78@gmail.com>
2025-03-17 17:10:32 +0300

    GetWeatherJob: call get-queue-size and get-weather

---
+++ app/Jobs/GetWeatherJob.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Artisan;
+
+class GetWeatherJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Artisan::call('app:get-queue-size');
+        Artisan::call('app:get-weather');
+    }
+}
