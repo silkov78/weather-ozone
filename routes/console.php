ab7ed07b808e53ba31821bfc16b7be673bfb69b6
silkov78 <piotr.silkov78@gmail.com>
2025-03-17 15:06:57 +0300

    command by schedule

--- routes/console.php
+++ routes/console.php
@@ -3,8 +3,12 @@
 use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use \Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     /** @var ClosureCommand $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule
+Schedule::command('app:get-weather')->everyTenSeconds();
