8b95624e6329de9e604e308260308e2dd1bb6ca0
silkov78 <piotr.silkov78@gmail.com>
2025-03-26 16:28:34 +0300

    ObservationFactory init

--- app/Models/Observation.php
+++ app/Models/Observation.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Observation extends Model
 {
+    use HasFactory;
+
     protected $table = 'observations';
     protected $fillable = [
         'datetime', 'temperature', 'cloud_cover', 'weather_code', 'ozone',
