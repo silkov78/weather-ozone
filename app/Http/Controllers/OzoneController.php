96c3f040e8fd087559ab187bfe8b667e21551e18
silkov78 <piotr.silkov78@gmail.com>
2025-04-02 13:13:12 +0300

    filterByDate in OzoneController.php

--- app/Http/Controllers/OzoneController.php
+++ app/Http/Controllers/OzoneController.php
@@ -1,14 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use App\Models\Observation;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 class OzoneController extends Controller
 {
-    public function index(): string
+    /*
+     * Возвращает последнее добавленное наблюдение.
+     */
+    public function index(): JsonResponse
     {
         $lastObservation = Observation::all()->last();
 
@@ -18,4 +24,27 @@ class OzoneController extends Controller
 
         return response()->json($lastObservation);
     }
+
+    /*
+     * Получает из запроса начальную и конечную дату и фильтрует данные по ним.
+     */
+    public function filterByDate(Request $request): JsonResponse
+    {
+        $request->validate([
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
+        ]);
+
+        $startDate = new \DateTime($request->input('start_date'));
+        $endDate = new \DateTime($request->input('end_date'));
+
+        $observations = Observation::whereBetween('datetime', [$startDate, $endDate])->get();
+
+        if (! $observations) {
+            return response()->json(['message' => 'No observations found'], 404);
+        }
+
+        return response()->json($observations);
+    }
+
 }
