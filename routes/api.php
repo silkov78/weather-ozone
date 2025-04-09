27458f107d80d2464b86dac92c3521501a46e927
silkov78 <piotr.silkov78@gmail.com>
2025-04-09 10:57:38 +0300

    restructure api.php

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,13 @@ use Illuminate\Support\Facades\Route;
 
 
 // api/v1/observations
-Route::controller(ObservationsApiController::class)->group(function () {
-    Route::prefix('v1/observations')->group(function () {
+Route::prefix('v1/observations')->group(function () {
+    Route::controller(ObservationsApiController::class)->group(function () {
         Route::name('observations.')->group(function () {
 
             Route::get('/', 'index')->name('index');
             Route::get('/filter', 'filterByDate')->name('filter');
+
         });
     });
 });
